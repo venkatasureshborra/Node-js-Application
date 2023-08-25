4270e5167a62f81253634280fd16321e818ed9b2
EC2 Default User <ec2-user@ip-172-31-7-236.ap-south-1.compute.internal>
2023-08-25 12:27:48 +0530

    sonar-name-update

--- sonar-project.js
+++ sonar-project.js
@@ -4,7 +4,7 @@ sonarqubeScanner({
 	serverUrl: 'http://172.31.4.74:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectName': 'NodeJS Application',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': 'squ_78a1f49e35bf969383b0d89b28f41df94d5056c8',
 	    //'sonar.login': 'admin',
